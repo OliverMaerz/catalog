5f97d4d47e6d8af58527ffb9ee8178cc598088e2
Oliver Maerz <github@olivermaerz.com>
2017-02-11 15:59:29 -0300

    Add database setup script

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -30,11 +30,8 @@ def signin():
         user = User.query.filter_by(email=form.email.data).first()
 
         if user and check_password_hash(user.password, form.password.data):
-
             session['user_id'] = user.id
-
             flash('Welcome %s' % user.name)
-
             return redirect(url_for('auth.home'))
 
         flash('Wrong email or password', 'error-message')
